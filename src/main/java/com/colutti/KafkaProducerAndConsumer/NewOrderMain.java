77a717fb33be7d332fc1ecd2da6ff63e8388625d
Vinicius Colutti <viniciuscolutti@hotmail.com>
2021-01-06 10:02:55 -0300

    creating producer

---
+++ src/main/java/com/colutti/KafkaProducerAndConsumer/NewOrderMain.java
@@ -0,0 +1,37 @@
+package com.colutti.KafkaProducerAndConsumer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderMain {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        var producer = new KafkaProducer<String, String>(properties());
+        var value = "123,121243,234232";
+        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
+        producer.send(record, (data, ex) ->{
+            if(ex != null){
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println("SUCESSO: "+data.topic() + ":::" + data.partition() + "/" + data.offset() + "/" + data.timestamp());
+        }).get();
+
+    }
+
+    private static Properties properties() {
+
+        var properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
+    }
+}
